72c21163606f8f5c8ef8db02a81d3f59901ae984
afvillegasm <anfevi86@hotmail.com>
2022-10-14 15:31:26 -0500

    first commit

---
+++ src/main/java/com/movieland/service/PocJavaMovielandRestApplication.java
@@ -0,0 +1,13 @@
+package com.movieland.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocJavaMovielandRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocJavaMovielandRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movieland/service/PocJavaMovielandRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieland.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocJavaMovielandRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
