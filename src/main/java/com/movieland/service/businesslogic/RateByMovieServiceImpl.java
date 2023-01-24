cf3d0ae8e57063b697ae7cdc68706ab9155b7e38
afvillegasm <anfevi86@hotmail.com>
2023-01-24 16:15:59 -0500

    movieland adding log4j configuration and store createdBy user taken from request for concurrent-rate endpoint

--- src/main/java/com/movieland/service/businesslogic/RateByMovieServiceImpl.java
+++ src/main/java/com/movieland/service/businesslogic/RateByMovieServiceImpl.java
@@ -4,9 +4,13 @@ import java.math.BigInteger;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
@@ -36,11 +40,19 @@ public class RateByMovieServiceImpl {
 	@Autowired
 	private MovieDao movieDao;
 	
+	private static final Logger logger = LogManager.getLogger(RateByMovieServiceImpl.class);
+	
 	@Transactional
 	public RateByMovieDTO rateMovie(final BigInteger movieId, RateByMovieDTO rateToApply) throws ServiceException {
 		
 		if(rateToApply == null || rateToApply.getRate() == null) {
 			throw new ServiceException(ServiceErrorCodes.VALIDATION_ERROR, env.getProperty("rate-by-movie.error.message.rate-mandatory"));
+		}
+		if (rateToApply.getRate() < Integer.parseInt(env.getProperty("movieland.service.minimum-allowed-rate-value"))
+				|| rateToApply.getRate() > Integer
+						.parseInt(env.getProperty("movieland.service.maximum-allowed-rate-value"))) {
+			throw new ServiceException(ServiceErrorCodes.VALIDATION_ERROR,
+					env.getProperty("rate-by-movie.error.message.allowed-rate-value"));
 		}
 		
 		final int maxMovieRatesPerDay = Integer.parseInt(env.getProperty("movieland.service.maximum-movie-rates-per-day"));
@@ -58,7 +70,11 @@ public class RateByMovieServiceImpl {
 			entity.setRatedAt(LocalDate.now());
 			entity.setRatedMovie(foundMovie.get());
 			entity.setStatus(StatusCodes.ACTIVE.getCode());
-			entity.setCreatedBy(env.getProperty("movieland.service.serveruser"));
+			if(Objects.nonNull(rateToApply.getAuditInfo()) &&  StringUtils.isNotBlank(rateToApply.getAuditInfo().getCreatedBy())) {
+				entity.setCreatedBy(rateToApply.getAuditInfo().getCreatedBy());
+			} else {
+				entity.setCreatedBy(env.getProperty("movieland.service.serveruser"));
+			}
 			entity.setCreatedAt(LocalDateTime.now());
 			entity = dao.save(entity);
 			
@@ -76,6 +92,15 @@ public class RateByMovieServiceImpl {
 		if(rateToApply == null || rateToApply.getRate() == null) {
 			throw new ServiceException(ServiceErrorCodes.VALIDATION_ERROR, env.getProperty("rate-by-movie.error.message.rate-mandatory"));
 		}
+		if (rateToApply.getRate() < Integer.parseInt(env.getProperty("movieland.service.minimum-allowed-rate-value"))
+				|| rateToApply.getRate() > Integer
+						.parseInt(env.getProperty("movieland.service.maximum-allowed-rate-value"))) {
+			throw new ServiceException(ServiceErrorCodes.VALIDATION_ERROR,
+					env.getProperty("rate-by-movie.error.message.allowed-rate-value"));
+		}
+		if(Objects.nonNull(rateToApply.getAuditInfo())) {
+			logger.info("Trying to rate a movie from user: " + rateToApply.getAuditInfo().getCreatedBy());
+		}
 		
 		final int maxMovieRatesPerDay = Integer.parseInt(env.getProperty("movieland.service.maximum-movie-rates-per-day"));
 		final Optional<Movie> foundMovie = movieDao.findById(movieId);
@@ -92,15 +117,21 @@ public class RateByMovieServiceImpl {
 			entity.setRatedAt(LocalDate.now());
 			entity.setRatedMovie(foundMovie.get());
 			entity.setStatus(StatusCodes.ACTIVE.getCode());
-			entity.setCreatedBy(env.getProperty("movieland.service.serveruser"));
+			if(Objects.nonNull(rateToApply.getAuditInfo()) &&  StringUtils.isNotBlank(rateToApply.getAuditInfo().getCreatedBy())) {
+				entity.setCreatedBy(rateToApply.getAuditInfo().getCreatedBy());
+			} else {
+				entity.setCreatedBy(env.getProperty("movieland.service.serveruser"));
+			}
 			entity.setCreatedAt(LocalDateTime.now());
 			entity = dao.save(entity);
+			
+			logger.info("movie " + foundMovie.get().getName() + " was successfuly rated as: " + rateToApply.getRate() + " by the user: " + entity.getCreatedBy());
 			
 			return DTOTransformer.toRateByMovieDTO(entity);
 			
 		} else {
 			throw new ServiceException(ServiceErrorCodes.NOT_FOUND_ERROR, env.getProperty("movie.error.message.notfound"));
-		}
+		}		
 		
 	}
 
